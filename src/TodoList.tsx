2a59f9e2094084c5b2a430ba28da5d03048f14f1
ElenaKuklina <cuklina.lena@gmail.com>
2024-01-11 00:13:44 +0300

    Sprint_02. Week_02.Lesson_6. Add: called the EditableSpan  universal component

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,6 +1,7 @@
 import React, {ChangeEvent, useState, KeyboardEvent} from 'react';
 import {FilterValuesType} from './App';
 import {AddItemForm} from "./AddItemForm";
+import {EditableSpan} from "./EditableSpan";
 
 export type TaskType = {
     id: string
@@ -78,7 +79,8 @@ export function Todolist(props: PropsType) {
 
                     return <li key={t.id} className={t.isDone ? "is-done" : ""}>
                         <input type="checkbox" onChange={onChangeHandler} checked={t.isDone}/>
-                        <span>{t.title}</span>
+                        <EditableSpan title={t.title}/>
+                        {/*<span>{t.title}</span>*/}
                         <button onClick={onClickHandler}>x</button>
                     </li>
                 })
