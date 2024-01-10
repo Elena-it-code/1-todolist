433be1eee62106a7939e72d5bcd2014e609c756d
ElenaKuklina <cuklina.lena@gmail.com>
2024-01-10 23:25:16 +0300

    Sprint_02. Week_02.Lesson_6. Add: We drew our universal component "AddItemForm" in Todolist.tsx inside Todolist to add a task by passing the addTaskHandler() callback.

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -46,16 +46,18 @@ export function Todolist(props: PropsType) {
     }*/
 
     const removeTodolist = () => props.removeTodolist(props.id)
-
     const onAllClickHandler = () => props.changeFilter("all", props.id);
     const onActiveClickHandler = () => props.changeFilter("active", props.id);
     const onCompletedClickHandler = () => props.changeFilter("completed", props.id);
+    const addTaskHandler = (newTitle: string) => { // функция посредник. Принимает id из props и newTitle, указали так, потому что данные которые приходят от детей родителю передается через callback(и)!!!
+        props.addTask(newTitle, props.id)
+    }
 
     return <div>
         <h3> {props.title}
             <button onClick={removeTodolist}>x</button>
         </h3>
-        <AddItemForm id={props.id} addTask={props.addTask}/>
+        <AddItemForm onClick={addTaskHandler}/>
         {/*<div>*/}
         {/*    <input value={title}*/}
         {/*           onChange={onChangeHandler}*/}
