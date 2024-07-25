9e67be7093222389bed63693c05d0b38a1fbaf37
ElenaKuklina <cuklina.lena@gmail.com>
2024-07-25 23:16:46 +0300

    Update AppWithRedux stories and ReduxStoreProviderDecorator

--- src/stories/AppWithRedux.stories.tsx
+++ src/stories/AppWithRedux.stories.tsx
@@ -1,80 +1,48 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
-import {action} from '@storybook/addon-actions'
+import {combineReducers, legacy_createStore} from "redux";
+import {tasksReducer} from "../state/tasks-reducer";
+import {todolistsReducer} from "../state/todolists-reducer";
+import {v1} from "uuid";
+import {AppRootStateType} from "../state/store";
+import {Provider} from "react-redux";
+import AppWithRedux from "../AppWithRedux";
+import {Meta, StoryObj} from "@storybook/react";
+import {ReduxStoreProviderDecorator} from "./decorators/ReduxStoreProviderDecorator";
 
-import { Button } from './Button';
-import {AddItemForm, AddItemFormPropsType} from "../AddItemForm";
-import React, {ChangeEvent, KeyboardEvent, useState} from "react";
-import {IconButton, TextField} from "@mui/material";
-import {AddBox} from "@mui/icons-material";
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
-const meta: Meta<typeof AddItemForm> = {
-  title: 'TODOLISTS/AddItemForm',
-  component: AddItemForm,
+
+
+const meta: Meta<typeof AppWithRedux> = {
+  // const meta: Объявляет константу meta.
+  //   Meta<typeof AppWithRedux>: Указывает тип meta как Meta с параметром типа AppWithRedux. Meta - это интерфейс,
+  //   который используется для определения метаданных истории в Storybook.
+  //   =: Присваивает объект метаданных переменной meta.
+  title: 'TODOLISTS/AppWithRedux', // Устанавливает заголовок истории в Storybook. Это путь, который будет отображаться в боковой панели Storybook, позволяя организовать истории в иерархии.
+  component: AppWithRedux, // Указывает, что AppWithRedux является основным компонентом для этой истории. Storybook будет использовать этот компонент для рендеринга истории.
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: 'centered',
+    layout: 'centered', // Устанавливает параметр макета для Storybook. В данном случае компонент будет центрирован в Canvas Storybook. Это параметр оформления, который влияет на то, как компонент отображается в Storybook.
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {
-    addItem: {
-      description: 'Clicked Button inside form', // дали описание, т.к. у нас в AddItemForm приходят props addItem: (title: string) => void
-      action: 'clicked' // ранее в версиях надо было создавть callback/action, чтобы показать какое-то действие. А в новой версии можем делать это сразу тут
-    }
-  },
+  tags: ['autodocs'], // Добавляет тег autodocs, который указывает, что для этого компонента будет автоматически сгенерирована документация в Storybook. Это полезно для создания документации без необходимости ручного написания.
+  decorators: [ReduxStoreProviderDecorator], // пробросили наш HOC для компоненты
 };
 
-export default meta;
-type Story = StoryObj<typeof AddItemForm>;
+export default meta; // Экспортирует meta как экспорт по умолчанию. Это позволяет Storybook автоматически находить и использовать эти метаданные для настройки истории компонента AppWithRedux.
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const AddItemFormStory: Story = {};
+type Story = StoryObj<typeof AppWithRedux>;
+// Объявляет тип Story, который будет использоваться для создания историй.
+// StoryObj<typeof AppWithRedux> - это тип, который описывает объект истории для компонента AppWithRedux.
+// StoryObj - это интерфейс, предоставляемый Storybook, который используется для типизации объектов историй.
+//
+// Этот код обеспечивает корректную настройку и типизацию компонента AppWithRedux для использования в Storybook,
+// что упрощает его тестирование и документирование.
 
-const AddItemFormWithError = (props: AddItemFormPropsType) => {
-  let [title, setTitle] = useState("")
-  let [error, setError] = useState<string | null>("Title is required")
 
-  const addItem = () => {
-    if (title.trim() !== "") {
-      props.addItem(title);
-      setTitle("");
-    } else {
-      setError("Title is required");
-    }
-  }
+// В данном коде создается история для компонента `AppWithRedux` с использованием провайдера `Provider` из `react-redux`,
+// чтобы обернуть компонент и передать ему Redux store.
+export const AppWithReduxStory: Story = {};
 
-  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    setTitle(e.currentTarget.value)
-  }
 
-  const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
-    if (error !== null) { // указали setError(null) только, если текущая error !== null.
-      setError(null);
-    }
-    if (e.charCode === 13) {
-      addItem();
-    }
-  }
 
-  return <div>
-    <TextField variant="outlined"
-               error={!!error}
-               value={title}
-               onChange={onChangeHandler}
-               onKeyPress={onKeyPressHandler}
-               label="Title"
-               helperText={error}
-    />
-    <IconButton color="primary" onClick={addItem}>
-      <AddBox />
-    </IconButton>
-  </div>
-}
 
-export const AddItemFormWithErrorStory: Story = {
-  render: () => <AddItemFormWithError addItem={()=>{action('Clicked Button inside form')}} />
-};
+
+
 
--- src/stories/decorators/ReduxStoreProviderDecorator.tsx
+++ src/stories/decorators/ReduxStoreProviderDecorator.tsx
@@ -1,77 +1,38 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
-import {action} from '@storybook/addon-actions'
-
-import { Button } from './Button';
-import {AddItemForm, AddItemFormPropsType} from "../AddItemForm";
-import React, {ChangeEvent, KeyboardEvent, useState} from "react";
-import {IconButton, TextField} from "@mui/material";
-import {AddBox} from "@mui/icons-material";
-import AppWithRedux from "../AppWithRedux";
-import {Provider} from "react-redux";
-import {store} from "../state/store";
+import React from 'react'
+import { Provider } from 'react-redux';
+import {combineReducers, legacy_createStore} from "redux";
+import {tasksReducer} from "../../state/tasks-reducer";
+import {todolistsReducer} from "../../state/todolists-reducer";
+import {v1} from "uuid";
 
 
-const meta: Meta<typeof AppWithRedux> = {
-  // const meta: Объявляет константу meta.
-  //   Meta<typeof AppWithRedux>: Указывает тип meta как Meta с параметром типа AppWithRedux. Meta - это интерфейс,
-  //   который используется для определения метаданных истории в Storybook.
-  //   =: Присваивает объект метаданных переменной meta.
-    title: 'TODOLISTS/AppWithRedux', // Устанавливает заголовок истории в Storybook. Это путь, который будет отображаться в боковой панели Storybook, позволяя организовать истории в иерархии.
-  component: AppWithRedux, // Указывает, что AppWithRedux является основным компонентом для этой истории. Storybook будет использовать этот компонент для рендеринга истории.
-  parameters: {
-    layout: 'centered', // Устанавливает параметр макета для Storybook. В данном случае компонент будет центрирован в Canvas Storybook. Это параметр оформления, который влияет на то, как компонент отображается в Storybook.
-  },
-  tags: ['autodocs'], // Добавляет тег autodocs, который указывает, что для этого компонента будет автоматически сгенерирована документация в Storybook. Это полезно для создания документации без необходимости ручного написания.
+const rootReducer = combineReducers({
+  tasks: tasksReducer,
+  todolists: todolistsReducer
+})
+
+const initialGlobalState = {
+  todolists: [
+    {id: "todolistId1", title: "What to learn", filter: "all"},
+    {id: "todolistId2", title: "What to buy", filter: "all"}
+  ] ,
+  tasks: {
+    ["todolistId1"]: [
+      {id: v1(), title: "HTML&CSS", isDone: true},
+      {id: v1(), title: "JS", isDone: false}
+    ],
+    ["todolistId2"]: [
+      {id: v1(), title: "Milk", isDone: false},
+      {id: v1(), title: "React Book", isDone: true}
+    ]
+  }
 };
 
-export default meta; // Экспортирует meta как экспорт по умолчанию. Это позволяет Storybook автоматически находить и использовать эти метаданные для настройки истории компонента AppWithRedux.
-type Story = StoryObj<typeof AppWithRedux>;
-// Объявляет тип Story, который будет использоваться для создания историй.
-// StoryObj<typeof AppWithRedux> - это тип, который описывает объект истории для компонента AppWithRedux.
-// StoryObj - это интерфейс, предоставляемый Storybook, который используется для типизации объектов историй.
-//
-// Этот код обеспечивает корректную настройку и типизацию компонента AppWithRedux для использования в Storybook,
-// что упрощает его тестирование и документирование.
+export const storyBookStore = legacy_createStore(rootReducer, initialGlobalState as any); // тип any не подходит временно установила. Нужно as AppRootStateType
+// export const storyBookStore = legacy_createStore(rootReducer, initialGlobalState as AppRootStateType);
 
-
-// В данном коде создается история для компонента `AppWithRedux` с использованием провайдера `Provider` из `react-redux`,
-// чтобы обернуть компонент и передать ему Redux store.
-export const AppWithReduxStory: Story = {
-  render: ()=> <Provider store={store}><AppWithRedux/></Provider>
-};
-
-
-// Разберем каждую строку:
-//
-// export const AppWithReduxStory: Story = {
-//   render: () => <Provider store={store}><AppWithRedux/></Provider>
-// };
-// ```
-//
-// 1. **`export const AppWithReduxStory: Story = {`:**
-//
-// Эта строка объявляет и экспортирует константу `AppWithReduxStory`. Эта константа имеет тип `Story`, что указывает на то,
-//     что она представляет собой историю компонента для использования с инструментами, в нашем случае Storybook.
-//
-// 2. **`render: () => <Provider store={store}><AppWithRedux/></Provider>`:**
-//
-// - **`render:`**: Эта часть кода определяет свойство `render` объекта `AppWithReduxStory`. Это свойство содержит функцию,
-//     которая рендерит компонент.
-//
-// - **`() => <Provider store={store}><AppWithRedux/></Provider>`**: Стрелочная функция, которая возвращает JSX-элемент.
-//
-// - **`<Provider store={store}>`**: `Provider` - это компонент из библиотеки `react-redux`. Он используется для того,
-//     чтобы предоставить Redux store всему дереву компонентов ниже него. Здесь `store` - это объект хранилища (store),
-//     который был ранее создан и настроен.
-//
-// - **`<AppWithRedux/>`**: Это компонент, который будет обернут в `Provider`, чтобы иметь доступ к Redux store.
-//
-//     В итоге, стрелочная функция возвращает компонент `AppWithRedux`, обернутый в `Provider` с переданным `store`,
-//     что позволяет `AppWithRedux` и всем его дочерним компонентам получать доступ к Redux store.
-//
-//     Таким образом, этот код создает историю для компонента `AppWithRedux`, обеспечивая его доступ к Redux store
-// через `Provider`. Это особенно полезно в контексте тестирования и разработки с использованием инструментов,
-//     таких как Storybook, где необходимо видеть компонент в изоляции, но в контексте реального окружения приложения.
+export const ReduxStoreProviderDecorator = (storyFn: () => React.ReactNode) => {
+  return <Provider store={storyBookStore}>{storyFn()}</Provider>
+}
 
 
