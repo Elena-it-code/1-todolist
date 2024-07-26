31ae278572c149c785eec5e3dd147fa923fa10f9
ElenaKuklina <cuklina.lena@gmail.com>
2024-07-26 17:58:17 +0300

    Add Snapshot test for AddItemForm & update files integration/addItemForm.test.js и integration/jest.config.js

--- integration/jest.config.js
+++ integration/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     preset: 'jest-puppeteer',
     testRegex: './*.test.js$',
-    setupFilesAfterEnv: ['./setupTests.js']
+    setupFilesAfterEnv: ['./setupTests.js'],
+    //testTimeout: 5000  // Установка тайм-аута в 5000 мс (5 секунд), не всегд браузер успевает загрузить, а snapshot уже сделал снимок. Таким образом изменили время по умолчанию.
 };
